91205f1746c14771bb93a6bed467298e0939ae9a
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-05-01 21:47:10 +0200

    fixing github issues

--- disney-clone/src/component/Header.jsx
+++ disney-clone/src/component/Header.jsx
@@ -19,12 +19,20 @@ function Header() {
           <img src="/images/watchlist-icon.svg" alt="watchlist icon" />
           <span>WATCHLIST</span>
         </a>
-
         <a href>
           <img src="/images/original-icon.svg" alt="orginal icon" />
           <span>ORIGINALS</span>
         </a>
+        <a href>
+          <img src="/images/movie-icon.svg" alt="movie icon" />
+          <span>MOVIES</span>
+        </a>
+        <a href>
+          <img src="/images/series-icon.svg" alt="series icon" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
+      <UserImg src="https://media-exp1.licdn.com/dms/image/C4D03AQF5_otvQphnRQ/profile-displayphoto-shrink_800_800/0/1616328484082?e=1625097600&v=beta&t=uUf0k79oe1b8u7U-s40PkpVUoDYMCxpZjvIVqOI6dGs" />
     </Nav>
   );
 }
@@ -45,12 +53,14 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
   display: flex;
+  flex: 1;
+  margin-left: 25px;
 
   a {
     display: flex;
     align-items: center;
     padding: 0 12px;
-
+    cursor: pointer;
     img {
       height: 20px;
     }
@@ -58,6 +68,34 @@ const NavMenu = styled.div`
     span {
       font-size: 13px;
       letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        background: white;
+        opacity: 0;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        transform: scaleX(0);
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
     }
   }
+`;
+
+const UserImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+  align-items: center;
 `;
