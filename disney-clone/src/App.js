bacb53868f65bc033ef8eeb4c499107efc5f79f3
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-05-01 20:57:31 +0200

    Working on the navbar icons

---
+++ disney-clone/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./component/Header";
+import Home from "./component/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
