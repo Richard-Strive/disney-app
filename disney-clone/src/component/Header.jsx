6959c0c788e3b7852670338f1b03ad847a8802d0
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-05-02 15:33:19 +0200

    added slider

--- disney-clone/src/component/Header.jsx
+++ disney-clone/src/component/Header.jsx
@@ -79,6 +79,7 @@ const NavMenu = styled.div`
         bottom: -6px;
         background: white;
         opacity: 0;
+        transform-origin: left center;
         transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
         transform: scaleX(0);
       }
